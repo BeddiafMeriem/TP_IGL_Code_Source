b7b66264f11147382fa09f3bc7bb7325dda94ff2
BeddiafMeriem <32796498+BeddiafMeriem@users.noreply.github.com>
2017-10-19 02:12:02 +0300

    ajout de la méthode inverser

--- VectorHelp.java
+++ VectorHelp.java
@@ -23,3 +23,16 @@ public class VectorHelp {
 		  return tab;
 		}
 	}
+
+        public int[] inverser(int[] tab)
+	{
+		int i;
+		int taille = tab.length;
+		for(i=0 ; i<taille/2 ; i++)
+		{
+			int interm = tab[i];
+			tab[i] = tab[taille-1-i];
+			tab[taille-1-i] = interm;
+		}
+		return tab;
+	}
